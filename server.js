8f211591366119796a0a4c6c7a22152b6e0969e9
Sebastian Lis <sebastian.a.lis@gmail.com>
2016-04-09 13:02:56 +0200

    Update port for heroku

--- server.js
+++ server.js
@@ -1,26 +1,34 @@
-var express = require('express');
-var app = express();
-var PORT = process.env.port;
-
-var middleware = require('./middleware');
-
-
-app.use(middleware.logger);
-//app.use(middleware.requireAuthentication);
-
-//app.get('/', function(req, res)
+//var express = require('express');
+//var app = express();
+//var PORT = process.env.port;
+//
+//var middleware = require('./middleware');
+//
+//
+//app.use(middleware.logger);
+////app.use(middleware.requireAuthentication);
+//
+////app.get('/', function(req, res)
+////{
+////    res.send('Hello Express!');
+////})
+//
+//app.get('/about', middleware.requireAuthentication, function(req, res)
 //{
-//    res.send('Hello Express!');
+//    res.send('About us!');
 //})
+//
+//app.use(express.static(__dirname+'/public'));
+//
+//app.listen(PORT, function()
+//{
+//    console.log('Express server started at port: ' + PORT +'!');
+//});
+var http = require("http");
+var PORT = process.env.PORT;
 
-app.get('/about', middleware.requireAuthentication, function(req, res)
-{
-    res.send('About us!');
-})
-
-app.use(express.static(__dirname+'/public'));
-
-app.listen(PORT, function()
-{
-    console.log('Express server started at port: ' + PORT +'!');
+var server = http.createServer(function(req, res){
+        rew.end("hello world!! HEROKU");
 });
+
+server.listen(PORT);
