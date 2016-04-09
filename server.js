02a4853fb90f5703d1c1ef0237fc1df980fd129c
Sebastian Lis <sebastian.a.lis@gmail.com>
2016-04-09 14:03:53 +0200

    Update

--- server.js
+++ server.js
@@ -1,34 +1,26 @@
-//var express = require('express');
-//var app = express();
-//var PORT = process.env.port;
-//
-//var middleware = require('./middleware');
-//
-//
-//app.use(middleware.logger);
-////app.use(middleware.requireAuthentication);
-//
-////app.get('/', function(req, res)
-////{
-////    res.send('Hello Express!');
-////})
-//
-//app.get('/about', middleware.requireAuthentication, function(req, res)
+var express = require('express');
+var app = express();
+var PORT = process.env.port;
+
+var middleware = require('./middleware');
+
+
+app.use(middleware.logger);
+//app.use(middleware.requireAuthentication);
+
+//app.get('/', function(req, res)
 //{
-//    res.send('About us!');
+//    res.send('Hello Express!');
 //})
-//
-//app.use(express.static(__dirname+'/public'));
-//
-//app.listen(PORT, function()
-//{
-//    console.log('Express server started at port: ' + PORT +'!');
-//});
-var http = require("http");
-var PORT = process.env.PORT;
 
-var server = http.createServer(function(req, res){
-        req.end("hello world!! HEROKU");
+app.get('/about', middleware.requireAuthentication, function(req, res)
+{
+    res.send('About us!');
+})
+
+app.use(express.static(__dirname+'/public'));
+
+app.listen(PORT, function()
+{
+    console.log('Express server started at port: ' + PORT +'!');
 });
-
-server.listen(PORT);
